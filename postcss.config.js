b63fb84dbf9aa04ff55e12e70ce7762e22c0f32e
luohao <136065160@qq.com>
2021-06-21 23:32:29 +0800

    添加一个layout布局

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
     require('postcss-import'),
+    require('tailwindcss')("./app/javascript/plugins/tailwind/tailwind.config.js"),
+    require('autoprefixer'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
